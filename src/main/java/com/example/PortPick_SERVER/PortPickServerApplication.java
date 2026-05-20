5696d152d8498b493e070755a266c9a0a681d42c
jyuuuuu0 <s25023@gsm.hs.kr>
2026-05-20 19:43:59 +0900

    Initial commit

---
+++ src/main/java/com/example/PortPick_SERVER/PortPickServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PortPick_SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortPickServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortPickServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PortPick_SERVER/PortPickServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PortPick_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortPickServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
